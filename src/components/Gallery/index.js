5ec5eb7632fae632374a68271abd94d92b57d038
Michael <michael@thinkgrow.com>
2022-11-28 21:45:29 -0800

    form

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,28 +1,15 @@
-import React from "react";
+import React from 'react';
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
 
 function Gallery(props) {
-
-    const currentGallery = {
-        name: "commercial",
-        description: "Photos of grocery stores and more"
-    }
-
-    return (
-        <section>
-            <h1>{capitalizeFirstLetter(currentGallery.name)}</h1>
-            <p> {currentGallery.description}</p>
-            <div className="flex-row">
-                <img 
-                    src={photo}
-                    alt ="Commercial Example"
-                    className="img-thumbnail mx-1"
-                />
-
-            </div>
-        </section>
-    )
+  const { currentCategory } = props;
+  return (
+    <section>
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
+  );
 }
-
-export default Gallery
+export default Gallery;
