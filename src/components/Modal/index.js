dd3d5c36043deace0c4f38a3defa999690f6ceb2
Michael <michael@thinkgrow.com>
2022-11-29 12:02:50 -0800

    close modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-function Modal({currentPhoto}) {
+function Modal({onClose, currentPhoto}) {
 
     const {name, category, description, index} = currentPhoto;
 
@@ -13,9 +13,8 @@ function Modal({currentPhoto}) {
                 <p>
                 {description}
                 </p>
-                <button type="button">
-                Close this modal
-                </button>
+                <button onClick={onClose} type="button">Close this modal</button>
+
             </div>
         </div>
     );
